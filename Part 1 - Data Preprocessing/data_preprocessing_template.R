949b78fd0b9608e5a87f938581e6a9da3e60f244
Valentino Rusconi <vfrusconi@gmail.com>
2019-02-07 19:25:39 +0100

    :tada: categorical data in python

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -3,14 +3,11 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm =TRUE)),
+                        dataset$Salary)
